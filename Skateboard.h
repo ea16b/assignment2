89540488a13b1ada243cccb2442ab4b149f3ff19
ea16b <37161841+ea16b@users.noreply.github.com>
2019-10-04 10:04:02 -0400

    Create Skateboard.h
    Pushing Skateboard header file. Class Skateboard inherits from Vehicle.h and has no additional member data.

---
+++ Skateboard.h
@@ -0,0 +1,19 @@
+//Created by Eliza Anderson
+
+#ifndef DRIVINGSIMULATOR_SKATEBOARD_H
+#define DRIVINGSIMULATOR_SKATEBOARD_H
+
+#include "Vehicle.h"
+
+class Skateboard : public Vehicle
+{
+	public:
+		explicit Skatebaord (string brand, string model);
+
+		virtual ~Skateboard();
+		virtual double mileageEstimate(double time);
+
+		virtual string toString();
+};
+
+#endif //DRIVINGSIMULATOR_SKATEBOARD_H
